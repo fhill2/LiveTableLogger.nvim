c2e67ef714d4336704b31e6b3a6e142592a8a041
fhill2 <freddiehill000@gmail.com>
2021-04-08 13:10:36 -0700

    pin v3

--- lua/tests/window-matrix.lua
+++ lua/tests/window-matrix.lua
@@ -13,7 +13,7 @@ end
 
 
 
-local function is_window_scoped(popup_custom_opts) if popup_opts.relative == 'win' then return true elseif popup_opts.relative == 'editor' then return false end end
+--local function is_window_scoped(popup_opts, popup_custom_opts) if popup_opts.relative == 'win' then return true elseif popup_opts.relative == 'editor' then return false end end
 
 
 local function ternary(condition, if_true, if_false)
@@ -23,18 +23,17 @@ end
 
 
 local function apply_pin(colrow, popup_opts, popup_custom_opts)
-
  
 if popup_custom_opts.pin == nil then return false end
 
-local max_columns, max_row = popup_opts.max_col, popup_opts.max_row
+local max_col, max_row = popup_custom_opts.max_col, popup_custom_opts.max_row
 local width, height = popup_opts.width, popup_opts.height
 
 local choose_pin = {
 top = ternary(colrow,false,1), 
-topright = ternary(colrow, max_columns - width, 1),
-right =  ternary(colrow, max_columns - width,false), 
-bottomright = ternary(colrow, max_columns - width, max_row - height),
+topright = ternary(colrow, max_col - width, 1),
+right =  ternary(colrow, max_col - width,false), 
+bottomright = ternary(colrow, max_col - width, max_row - height),
 bottom = ternary(colrow,false, max_row - height), -- -1 for statusline
 bottomleft = ternary(colrow, 1, max_row - height), -- -1 for statusline
 left = ternary(colrow,1,false), 
@@ -47,57 +46,48 @@ end
 
 local popup_custom_opts = {
 layout = 'vertical',
-pin = 'right',
+pin = 'left',
 log = true,
 --scope = 'window',
 grow = true,
-grow_side = 'smart'
+grow_side = 'smart',
 }
-
-
-if popup_custom_opts.scope == 'window' then
-cwin = {}
-yx = vim.api.nvim_call_function('win_screenpos', {0}) 
-cwin_col, cwin_row = yx[2], yx[1]
-cwin_width = vim.api.nvim_call_function('winwidth', {0})
-cwin_height = vim.api.nvim_call_function('winheight', {0})
-
-cwin = {
-col = cwin_col,
-row = cwin_row,
-width = cwin_width,
-height = cwin_height,
-max_col = cwin_row + cwin_width,
-max_row = cwin_col + cwin_height,
-}
-
-end
-
-lo('cwin is: ') 
-lo(cwin)
---vim.api.nvim_open_win(0, false, cwin)
-
 
 local start_popup_opts = {
 col = 0.4,
 row = 0.4,
 width = 40,
 height = 40,
-max_col = ternary(is_window_scoped(popup_custom_opts), cwin.max_col -1, vim.o.columns -1),
-max_row = ternary(is_window_scoped(popup_custom_opts), cwin.max_row , vim.o.lines),
-relative = 'win'
+relative = 'editor'
 }
 
+-- decide max_col, max_row dependant on win or global, for pin
+if start_popup_opts.relative == 'win' then
+local cwin_max_col = vim.api.nvim_call_function('winwidth', {0})
+local cwin_max_row = vim.api.nvim_call_function('winheight', {0})
+start_popup_opts.win = vim.api.nvim_get_current_win()
+popup_custom_opts.max_col = cwin_max_col
+popup_custom_opts.max_row = cwin_max_row
+else
+popup_custom_opts.max_col = vim.o.columns
+popup_custom_opts.max_row = vim.o.lines
+end
 
 
+
+
+
+--lo('cwin is: ') 
+--lo(cwin)
+--vim.api.nvim_open_win(0, false, cwin)
+
+--start_popup_opts.max_col = ternary(is_window_scoped(start_popup_opts, popup_custom_opts), cwin.max_col, vim.o.columns -1)
+--start_popup_opts.max_row = ternary(is_window_scoped(start_popup_opts, popup_custom_opts), cwin.max_row , vim.o.lines)
 
 
 -- vertical test
-local col = apply_pin(true, start_popup_opts, popup_custom_opts) or start_popup_opts.max_col * start_popup_opts.col
-lo('after pin: ')
-lo(col)
-
-local row = apply_pin(false, start_popup_opts, popup_custom_opts) or start_popup_opts.max_row * start_popup_opts.row
+local col = apply_pin(true, start_popup_opts, popup_custom_opts) or popup_custom_opts.max_col * start_popup_opts.col
+local row = apply_pin(false, start_popup_opts, popup_custom_opts) or popup_custom_opts.max_row * start_popup_opts.row
 
 -- transformation values for pin
 local popup_opts = {
@@ -105,11 +95,13 @@ local popup_opts = {
   row = row,
   width = start_popup_opts.width,
   height = start_popup_opts.height,
-  relative = 'editor',
+  relative = start_popup_opts.relative,
+  win = start_popup_opts.win,
   anchor = 'NW'
 } 
 
-
+-- if popup_opts.relative == 'win' then
+-- end
 --lo('popup_opts at start is: ')
 --lo(popup_opts)
 
@@ -118,12 +110,12 @@ local popup_opts = {
 
 local function create_window(popup_opts, popup_custom_opts, objlog)
 
-local border = require 'plenary/window/border'
+
 
 
 local popup_opts = vim.deepcopy(popup_opts) -- important
-lo('popup_opts start of create window: ')
-lo(popup_opts)
+--lo('popup_opts start of create window: ')
+--lo(popup_opts)
 local round = function(n) return math.floor(n + 0.5) end
 
 
@@ -155,31 +147,65 @@ local bufnr1count = vim.tbl_count(bufnr1content)
 vim.api.nvim_buf_set_lines(bufnr1, 0, -1, true, bufnr1content)
 
 
-
+lo('before window creation')
+lo(popup_opts)
 
 local winnr = vim.api.nvim_open_win(bufnr1, false, popup_opts)
 --local winnr = border:new(bufnr1, winnr, popup_opts)
+
+-- create border
+--  obj.bufnr = vim.api.nvim_create_buf(false, true)
+--   assert(obj.bufnr, "Failed to create border buffer")
+--   vim.api.nvim_buf_set_option(obj.bufnr, "bufhidden", "wipe")
+
+-- local border_lines = border._create_lines(popup_opts, border_opts)
+
+
+--   obj.contents = Border._create_lines(content_win_options, border_win_options)
+--   vim.api.nvim_buf_set_lines(obj.bufnr, 0, -1, false, obj.contents)
+
+--   local thickness = border_win_options.border_thickness
+
+--  vim.api.nvim_buf_set_lines(bufnr1.content, 0, -1, false, obj.contents)
+
+
+
+
+
+
+
 return winnr, popup_opts
+
+
 end
 
 
 
 --if vim.g.winnr1 == nil and vim.g.winnr2 == nil then
 if popup_custom_opts.log then 
---lo('picked 2 windows')
-vim.g.winnr1, obj_opts = create_window(popup_opts, popup_custom_opts, true)
-vim.g.winnr2, log_opts = create_window(popup_opts, popup_custom_opts, false)
---lo('obj opts')
---lo(obj_opts)
---lo('log_opts')
---lo(log_opts)
-
+vim.g.winnr1, obj_opts = create_window(popup_opts, popup_custom_opts, true, cwin)
+vim.g.winnr2, log_opts = create_window(popup_opts, popup_custom_opts, false, cwin)
 else
 vim.g.winnr1, obj_opts = create_window(popup_opts, popup_custom_opts, true)
 end
 
 
 --obj_opts.height = 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 vim.defer_fn(function()
 
@@ -196,7 +222,9 @@ local prevwin = vim.api.nvim_get_current_win()
 if popup_custom_opts.grow == true and popup_custom_opts.grow_side == 'smart' then
 popup_custom_opts.grow_side = choose_grow_side(popup_custom_opts)
 end
-
+lo('before move')
+lo(obj_opts)
+lo(log_opts)
 if vim.g.winnr1 ~= nil then
 vim.api.nvim_set_current_win(vim.g.winnr1)
 vim.api.nvim_win_set_config(vim.g.winnr1, obj_opts)
@@ -232,6 +260,20 @@ end, 1000)
 
 
 -- old
+--yx = vim.api.nvim_call_function('win_screenpos', {0}) 
+--cwin_col, cwin_row = yx[2], yx[1]
+
+-- cwin = {
+-- --col = cwin_col,
+-- --row = cwin_row,
+-- width = cwin_width,
+-- height = cwin_height,
+-- max_col = cwin_row + cwin_width,
+-- max_row = cwin_col + cwin_height,
+-- }
+
+
+
 --end
 -- horizontal test
 -- local col = is_pin(true, popup_custom_opts.pin, width, height) or vim.o.columns * 0.5
